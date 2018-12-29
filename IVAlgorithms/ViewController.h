728af85fc729367ee9009a4bf675f877a361a395
Joelixy <hz504t@gmail.com>
2018-12-29 23:48:03 +0800

    init Proj

---
+++ IVAlgorithms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IVAlgorithms
+//
+//  Created by 李向阳 on 2018/12/29.
+//  Copyright © 2018 JoeLixy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IVAlgorithms/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IVAlgorithms
+//
+//  Created by 李向阳 on 2018/12/29.
+//  Copyright © 2018 JoeLixy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
