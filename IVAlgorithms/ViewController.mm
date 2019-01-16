60567d70dcf3e14fb3fef134a69373e5f0fdc5f5
Joelixy <hz504t@gmail.com>
2019-01-16 17:49:21 +0800

    add finish

--- IVAlgorithms/ViewController.mm
+++ IVAlgorithms/ViewController.mm
@@ -11,6 +11,10 @@
 #import "IVMath.hpp"
 #import "IVString.hpp"
 
+#import "IVArray.hpp"
+#import "IVTree.hpp"
+#import "IVLinkList.hpp"
+
 @interface ViewController ()
 
 @end
@@ -24,6 +28,8 @@
     [self interview_math];
     
     [self interview_string];
+    
+    [self interview_array];
 }
 
 - (void)interview_sort {
@@ -60,6 +66,37 @@
     char numStr[6] = " -352";
     int number = string.convertStringToNumber(numStr);
     NSLog(@"number:%d", number);
+}
+
+- (void)interview_array {
+    int array1[9] = {9, 5, 7, 2, 5, 3, 1, 4, 6};
+    int duplicate = IVArray::anyDuplicateNumFromUnsortArray(array1, 9);
+    
+    int twoDimension[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    bool isExits = IVArray::twoDimensionArray(*twoDimension, 4, 4, 16);
+    
+    int minArray[8] = {6, 7, 8, 9, 2, 3, 4, 5}; // 特殊情况 {1, 3, 4, 5, 1, 1};
+    int minNum = IVArray::minNumOfReverseArray(minArray, 8);
+    
+    int array2[10] = {1, 2, 2, 2, 2, 3, 3, 2, 4, 4};
+    int value = IVArray::overHalfNumInArray(array2, 10);
+    
+    int array3[9] = {5, 6, 2, 8, 3, 4, 7, 9, 1};
+    int firstKNumber = IVArray::numberOfMaxNumInArray(array3, 9, 5);
+    
+    int array4[12] = {2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 8, 9};
+    int count = IVArray::countOfSortedArray(array4, 12, 5);
+    
+    int array5[9] = {1, 8, 9, 0, 3, 5, 6, 4, 7};
+    IVArray::ajustPosition(array5, 9);
+}
+
+- (void)interview_linkList {
+    
+}
+
+- (void)interview_tree {
+    
 }
 
 @end
